7ccc1905de2fb4cd4e4e1335f2bb85160652b25c
CR BVT <crbeuvelot@gmail.com>
2023-05-11 17:02:08 +0200

    trying to put db with keycloak

--- django_node/django_source/settings.py
+++ django_node/django_source/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'app_db',
-        'PORT': 5432,
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
