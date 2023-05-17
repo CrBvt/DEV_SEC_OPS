d65aa3a349929ef4d07e86808043f47ed5ce9f84
CR BVT <crbeuvelot@gmail.com>
2023-05-17 10:39:54 +0200

    add logged user fixture

--- django_node/notes/tests/test_notes_views.py
+++ django_node/notes/tests/test_notes_views.py
@@ -4,14 +4,18 @@ from django.contrib.auth.models import User
 
 from notes.models import Notes
 
-@pytest.mark.django_db
-def test_list_endpoint_returns_user_notes(client):
-
+@pytest.fixture
+def logged_user(client):
     user = User.objects.create_user('user', 'user@example.com', 'password')
     client.login(username=user.username, password='password')
 
-    note = Notes.objects.create(title='An interesting title', text='', user=user)
-    note = Notes.objects.create(title='A new title', text='', user=user)
+    return user
+
+@pytest.mark.django_db
+def test_list_endpoint_returns_user_notes(client, logged_user):
+
+    note = Notes.objects.create(title='An interesting title', text='', user=logged_user)
+    note = Notes.objects.create(title='A new title', text='', user=logged_user)
 
     response = client.get(path='/smart/notes', follow=True)
     assert 200 == response.status_code
@@ -21,16 +25,13 @@ def test_list_endpoint_returns_user_notes(client):
     assert 2 == content.count('<h3>')
 
 @pytest.mark.django_db
-def test_list_endpoint_returns_only_user_notes_from_authenticated_user(client):
+def test_list_endpoint_returns_only_user_notes_from_authenticated_user(client, logged_user):
 
     another_user = User.objects.create_user('another_user', 'another@example.com', 'password')
     Notes.objects.create(title='Fake Note', text='', user=another_user)
 
-    user = User.objects.create_user('user', 'user@example.com', 'password')
-    client.login(username=user.username, password='password')
-
-    Notes.objects.create(title='An interesting title', text='', user=user)
-    Notes.objects.create(title='A new title', text='', user=user)
+    Notes.objects.create(title='An interesting title', text='', user=logged_user)
+    Notes.objects.create(title='A new title', text='', user=logged_user)
 
     response = client.get(path='/smart/notes', follow=True)
     assert 200 == response.status_code
