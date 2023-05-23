1e16c34a5ecd0f2173507d08fb324fda81a37496
CR BVT <crbeuvelot@gmail.com>
2023-05-23 15:43:32 +0200

    update scripts

--- build.bat
+++ build.bat
@@ -1,1 +1,20 @@
-docker-compose up --build
+REM Start Docker
+docker-compose up --build -d
+
+REM Wait for Docker
+timeout 30
+
+REM Migrations
+docker-compose exec django_node sh -c "python manage.py makemigrations"
+docker-compose exec django_node sh -c "python manage.py migrate"
+
+REM Tests
+docker exec django_node sh -c "pytest"
+
+REM Code Quality
+docker exec django_node sh -c "pylint"
+
+REM Documentation
+docker exec django_node sh -c "cd doc && make html"
+
+
--- start.bat
+++ start.bat
@@ -1,1 +1,6 @@
 docker-compose up --build
+
+timeout 30
+
+docker-compose exec django_node sh -c "python manage.py makemigrations"
+docker-compose exec django_node sh -c "python manage.py migrate"
